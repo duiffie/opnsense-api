db464c2fa310ebe736ac60de563a0ca8bcceac24
Marco van Duijvenbode <duiffie@users.noreply.github.com>
2025-09-30 11:55:11 +0200

    Create opncentral.py

---
+++ modules/opncentral.py
@@ -0,0 +1,13 @@
+from core.api import api_call
+import sys
+import json
+
+def listdevices(base_url, auth, proxies, debug=False):
+    return api_call('GET', f"{base_url}/opncentral/host_provisioning/list_devices", auth, proxies, debug=debug)
+
+def reconfigure(base_url, auth, proxies, debug=False):
+    devices = listdevices(base_url, auth, proxies, debug)
+    data = {"hosts": {}}
+    for index, item in enumerate(devices):
+        data["hosts"][index] = item["id"]
+    return api_call('POST', f"{base_url}/opncentral/host_provisioning/reconfigure", auth, proxies, data, debug=debug)
